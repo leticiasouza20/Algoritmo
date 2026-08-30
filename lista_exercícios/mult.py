c19e779281fe9c572353b479790c6867103e90b0
Leticia Souza <souzaleticiamga@gmail.com>
2026-08-30 19:43:52 -0300

    mult.py

---
+++ lista_exercícios/mult.py
@@ -0,0 +1,8 @@
+print ("Multiplicação")
+
+num1 = float(input("\n Digite o primeiro número:"))
+num2 = float(input("\n Digite o segundo número:"))
+
+mult = num1 * num2
+
+print ("\n A multiplicação é:", mult)
